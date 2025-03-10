b471952c81d18bf4e23c183aa195343f4e19d746
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2025-03-10 22:54:07 +0600

    done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBIe-PV8skn7ghT0Ehru_YjSQShT3Talfs",
-  authDomain: "dragon-news-clone-updated-25.firebaseapp.com",
-  projectId: "dragon-news-clone-updated-25",
-  storageBucket: "dragon-news-clone-updated-25.firebasestorage.app",
-  messagingSenderId: "26421693020",
-  appId: "1:26421693020:web:ad4d70a8b645e0460f7499"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,11 +17,11 @@ const Register = () => {
     const email = form.get("email");
     const photo = form.get("photo");
     const password = form.get("password");
-    console.log({ name, email, photo, password });
+    // console.log({ name, email, photo, password });
     createUser(email, password)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        // console.log(user);
         setUser(user);
         userProfileUpdate({
           displayName:name,
